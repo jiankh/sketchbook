cfb78617bbd3826f0bd1b426633612d279437067
jiankh <131821436+jiankh@users.noreply.github.com>
2023-09-04 13:07:22 -0400

    added event listener to mouse event down and up to handle the held down mouse click so we can draw consecutively without having to click every time.

--- script.js
+++ script.js
@@ -5,17 +5,25 @@ let screen = document.querySelector('.screen')
 let pixelQuantitySelection = document.querySelector('#pixel-option')
 let pixelQuantity = 16
 
+let isMouseDown = false
 //------------MAIN------------
-
-
 
 //Create initial grid of 16x16
 createGrid()
 
+
+//Event Listener to handle if the mouse is held down or not.
+screen.addEventListener('mousedown', () => {
+    isMouseDown = true
+})
+screen.addEventListener('mouseup', () => {
+    isMouseDown = false
+})
+
 //Event listener to update the Grid with appropriate Pixel Quanity.
 pixelQuantitySelection.addEventListener('change', (e) =>{
     pixelQuantity = parseInt(e.target.value)
-    screen.innerHTML = ''
+    screen.innerHTML = '' //clear the current grid
     createGrid()
 })
 
@@ -29,6 +37,12 @@ function createGrid() {
         pixel.classList.add('pixel')
         pixel.style.width = `${WIDTH / pixelQuantity}px`; 
         pixel.style.height = `${HEIGHT / pixelQuantity}px`;
+
+        pixel.addEventListener('mousemove', (e) => {
+            if (isMouseDown) {
+            e.target.classList.add('clicked') }
+        })
+
         screen.appendChild(pixel)
     }
 }
